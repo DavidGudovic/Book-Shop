18c9fc808e0f6a906344a283097973a5abb09d82
David <dgudovic1@gmail.com>
2022-08-05 17:40:58 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/views/**/*.js",
+    "./resources/**/*.vue",
+
+    //pagination
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
