9a82229941886be9a9c65ab9c378336ce6ab50c6
David <dgudovic1@gmail.com>
2022-08-05 21:57:24 +0200

    modified content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./resources/**/*.blade.php",
     "./resources/views/**/*.blade.php",
     "./resources/views/**/*.js",
     "./resources/**/*.vue",
