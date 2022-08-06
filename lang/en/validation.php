569701f37cc296bf91357883e21961e454686a9f
David <dgudovic1@gmail.com>
2022-08-06 18:10:10 +0200

    translated more rules

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -15,6 +15,7 @@ return [
 
     // Translated to Serbian
     'required' => 'Polje \':attribute\' je obavezno!',
+    'unique' => 'Ova vrednost već postoji!',
     // End translated
 
     'accepted' => 'The :attribute must be accepted.',
@@ -138,7 +139,6 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => 'The :attribute has already been taken.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
