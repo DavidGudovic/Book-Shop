c91c2c1b1bbe4ff155503791164a042d36aa0c1d
David <dgudovic1@gmail.com>
2022-08-06 18:13:28 +0200

    Moved validation from controllers

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+  
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+          'username' => 'required|unique:users,username',
+          'email' => 'required|email|unique:users,email',
+          'name' => 'required|max:40',
+          'lastname' => 'required|max:40',
+          'password' => 'required|confirmed',
+        ];
+    }
+}
