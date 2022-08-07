fd025b3de571980166b996832680633db0e299f5
David <dgudovic1@gmail.com>
2022-08-07 19:11:59 +0200

    formatting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,57 +2,59 @@
 
 @section('content')
   <!-- Login -->
-  <div class="flex items-center justify-center mt-20">
-    <div class="w-4/12 bg-white p-6 rounded-lg">
+  <div class="flex items-center justify-center min-h-[75vh]">
+    <div class="w-4/12 min-w-[400px] bg-white p-6 rounded-lg">
 
       <!--Status message display-->
       @if(session()->has('status'))
         <p class='text-center font-bold mb-5 @if(session('status') == 'error') text-red-600 @else text-green-400 @endif'>{{session('status_msg')}}</p>
-      @endif
-      <!-- End status message -->
+        @endif
+        <!-- End status message -->
 
-      <!--Login form-->
-      <form class="" action="{{route('login')}}" method="post">
-        <!--fields-->
-        <!-- Cross site request forgery -->
-       @csrf
+        <!--Login form-->
+        <form class="" action="{{route('login')}}" method="post">
+          <!--fields-->
+          <!-- Cross site request forgery -->
+          @csrf
 
-            <!--Username -->
+          <!--Username -->
           <div class="mb-4">
-             <label for='username' class='sr-only'>Korisničko ime</label>
-             <input type="text" name="username" id='username' placeholder="Unesite korisničko ime" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror' value='{{old('email')}}'>
-             @error('username')
-               <div class="text-red-500 mt-2 text-sm">
+            <label for='username' class='sr-only'>Korisničko ime</label>
+            <input type="text" name="username" id='username' placeholder="Unesite korisničko ime"
+            class='form-control @error('username') border-red-500 @enderror' value='{{old('email')}}'>
+              @error('username')
+                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
-               </div>
-             @enderror
-          </div>
-          <!-- Password -->
-          <div class="mb-4">
-             <label for= 'password' class='sr-only'>Lozinka</label>
-             <input type= 'password' name= 'password' id= 'password' placeholder="Unesite lozinku" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror' >
-             @error('password')
-               <div class="text-red-500 mt-2 text-sm">
-                  {{$message}}
-               </div>
-             @enderror
-          </div>
-           <!-- remember me -->
-          <div class="mb-4">
-            <div class="flex items-center">
-                <input type="checkbox" name="remember" class="mr-2" id='remember'>
-                <label for='remember'>Zapamti me!</label>
+                </div>
+              @enderror
             </div>
+            <!-- Password -->
+            <div class="mb-4">
+              <label for= 'password' class='sr-only'>Lozinka</label>
+              <input type= 'password' name= 'password' id= 'password' placeholder="Unesite lozinku"
+              class='form-control @error('password') border-red-500 @enderror' >
+                @error('password')
+                  <div class="text-red-500 mt-2 text-sm">
+                    {{$message}}
+                  </div>
+                @enderror
+              </div>
+              <!-- remember me -->
+              <div class="mb-4">
+                <div class="flex items-center">
+                  <input type="checkbox" name="remember" class="mr-2" id='remember'>
+                  <label for='remember'>Zapamti me!</label>
+                </div>
+              </div>
+              <!-- end fields-->
+              <!-- Buttons -->
+              <div>
+                <button type="submit" class="form-btn">Ulogujte se!</button>
+              </div>
+              <!-- End buttons -->
+            </form>
+            <!-- End login form -->
           </div>
-          <!-- end fields-->
-          <!-- Buttons -->
-          <div>
-             <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Ulogujte se!</button>
-          </div>
-          <!-- End buttons -->
-      </form>
-      <!-- End login form -->
-    </div>
-  </div>
-  <!--End login -->
-@endsection
+        </div>
+        <!--End login -->
+      @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,11 +3,12 @@
 @section('content')
   <!-- Register -->
   <div class="flex justify-center mt-8">
-    <div class="w-4/12 bg-white p-6 m-3 rounded-lg">
+    <div class="w-4/12 min-w-[400px] bg-white p-6 m-3 rounded-lg">
 
       <!--Status message display-->
       @if(session()->has('status'))
-        <p class='text-center font-bold mb-5 @if(session('status') == 'error') text-red-600 @else text-green-400 @endif'>{{session('status_msg')}}</p>
+        <p class='text-center font-bold mb-5 @if(session('status') == 'error') text-red-600
+                                             @else text-green-400 @endif'>{{session('status_msg')}}</p>
       @endif
       <!-- End status message -->
 
@@ -21,7 +22,8 @@
             <!-- Username -->
           <div class="mb-4">
              <label for='username' class='sr-only'>Korisničko ime</label>
-             <input type="text" name="username" id='username' placeholder="Unesite korisničko ime" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror' value='{{old('username')}}'>
+             <input type="text" name="username" id='username' placeholder="Unesite korisničko ime"
+             class='form-control @error('username') border-red-500 @enderror' value='{{old('username')}}'>
              @error('username')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -31,7 +33,8 @@
             <!-- Email -->
           <div class="mb-4">
              <label for='email' class='sr-only'>Email</label>
-             <input type="text" name="email" id='email' placeholder="Unesite email" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror' value='{{old('email')}}'>
+             <input type="text" name="email" id='email' placeholder="Unesite email"
+             class='form-control @error('email') border-red-500 @enderror' value='{{old('email')}}'>
              @error('email')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -41,7 +44,8 @@
             <!-- Name -->
           <div class="mb-4">
              <label for='name' class='sr-only'>Ime</label>
-             <input type="text" name="name" id='name' placeholder="Unesite ime" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror' value='{{old('name')}}'>
+             <input type="text" name="name" id='name' placeholder="Unesite ime"
+             class='form-control @error('name') border-red-500 @enderror' value='{{old('name')}}'>
              @error('name')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -51,7 +55,8 @@
             <!-- Last name -->
           <div class="mb-4">
              <label for='lastname' class='sr-only'>Prezime</label>
-             <input type="text" name="lastname" id='lastname' placeholder="Unesite prezime" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('lastname') border-red-500 @enderror' value='{{old('lastname')}}'>
+             <input type="text" name="lastname" id='lastname' placeholder="Unesite prezime"
+             class='form-control @error('lastname') border-red-500 @enderror' value='{{old('lastname')}}'>
              @error('lastname')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -61,7 +66,8 @@
             <!-- pass -->
           <div class="mb-4">
              <label for= 'password' class='sr-only'>Lozinka</label>
-             <input type= 'password' name= 'password' id= 'password' placeholder="Unesite lozinku" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror' >
+             <input type= 'password' name='password' id='password' placeholder="Unesite lozinku"
+               class='form-control @error('password') border-red-500 @enderror' >
              @error('password')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -70,8 +76,9 @@
           </div>
             <!--Confirm pass -->
           <div class="mb-4">
-             <label for= 'password_confirmation' class='sr-only'>Potvrda lozinke</label>
-             <input type= 'password' name= 'password_confirmation' id='password_confirmation' placeholder="Ponovite lozinku" class='bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-500 @enderror' >
+             <label for='password_confirmation' class='sr-only'>Potvrda lozinke</label>
+             <input type='password' name='password_confirmation' id='password_confirmation' placeholder="Ponovite lozinku"
+             class='form-control @error('password_confirmation') border-red-500 @enderror' >
              @error('password_confirmation')
                <div class="text-red-500 mt-2 text-sm">
                   {{$message}}
@@ -81,8 +88,8 @@
             <!-- End fields -->
             <!-- Buttons -->
           <div class="flex justify-evenly">
-             <button type="submit" class="bg-blue-500 text-white px-4 py-3 mx-2 rounded font-medium w-full">Registruj se!</button>
-             <button type="reset" class="bg-gray-500 text-white px-4 py-3 mx-2 rounded font-medium w-full">Resetuj!</button>
+             <button type="submit" class="form-btn">Registruj se!</button>
+             <button type="reset" class="form-btn">Resetuj!</button>
           </div>
             <!-- End buttons -->
       </form>
