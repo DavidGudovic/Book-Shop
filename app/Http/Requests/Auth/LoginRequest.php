c23384888b2a22d06ea60549fb54a969c19545be
David <dgudovic1@gmail.com>
2022-08-08 14:29:05 +0200

    fixed namespace

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -19,5 +19,5 @@ class LoginRequest extends FormRequest
           'password' => 'required',
         ];
     }
-    
+
 }
--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-  
+
     public function authorize()
     {
         return true;
