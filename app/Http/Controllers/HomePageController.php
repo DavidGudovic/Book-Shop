b00519b8a273e6c60c56fa7dca1dd5c2fcc6e784
David <dgudovic1@gmail.com>
2022-08-09 17:33:00 +0200

    added category filters

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -5,14 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Services\BookService;
+// use App\Services\CategoryService;
 
 // Handles calls to display Home Page
 class HomePageController extends Controller
 {
 
    // Gets relevant data and forwards it to home page view
-    public function index(BookService $bookService){
-      $recommends = $bookService->getRecommended();
-      return view('index', ['recommends' => $recommends]);
+ public function index(BookService $bookService /* , CategoryService $categoryService */){
+      $recommends = $bookService->getRecommended(3);
+      /*
+      $fictionCategories = $categoryService->getFiction();
+      $nonfictionCategories = $categoryService->getNonFiction();
+      */
+    return view('index', ['recommends' => $recommends]);
+                        /*
+                        ,'fictionCategories' => $fictionCategories
+                        ,'nonfictionCategories' => $nonfictionCategories
+                        */
     }
 }
