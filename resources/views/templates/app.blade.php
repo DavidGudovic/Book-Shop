9befcf9df2a25e7f0ef667514707ee0137426232
David <dgudovic1@gmail.com>
2022-08-09 17:18:08 +0200

    Edited comments

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -4,31 +4,36 @@
   <meta charset="utf-8">
   <meta name="author" content="David Gudović">
   <meta name="description" content="Online prodavnica knjizare Aurora">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Knjižara Aurora</title>
   <link rel="icon" href="{{URL('icon.svg')}}">
   @vite('resources/js/app.js')
 </head>
+   <body class="flex flex-col antialiased bg-gray-200 min-h-screen">
+     <!--HEADER OPTIONAL-->
+     @yield('header')
+     <!--END HEADER-->
+     <!--NAVBAR-->
+     @include('includes.navbar')
+     <!--END NAVBAR -->
 
-<body class="flex flex-col antialiased bg-gray-100 min-h-screen">
+     <!-- CONTENT -->
+     <div class="flex flex-row flex-1 justify-center items-center ">
+       <!-- SIDEBAR OPTIONAL-->
+       <aside>
+         @yield('sidebar')
+       </aside>
+       <!-- END SIDEBAR -->
+       <!-- MAIN CONTENT -->
+       <main>
+         @yield('content')
+       </main>
+       <!-- END MAIN CONTENT -->
+     </div>
+     <!-- END CONTENT -->
 
-    <!--HEADER OPTIONAL-->
-    @yield('header')
-    <!--END HEADER-->
-    @include('includes.navbar')
-
-<div class="flex  flex-row justify-center items-center flex-1">
-  <!-- SIDEBAR OPTIONAL-->
-  <aside>
-    @yield('sidebar')
-  </aside>
-  <!-- END SIDEBAR -->
-  <!-- CONTENT -->
-  <main class="">
-    @yield('content')
-  </main>
-  <!-- END CONTENT -->
-</div>
-
-  @include('includes.footer')
-</body>
+     <!--FOOTER-->
+     @include('includes.footer')
+     <!--END FOOTER-->
+   </body>
 </html>
