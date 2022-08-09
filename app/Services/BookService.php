17769887917a597f22142c7a5b7b31b65a01bd26
David <dgudovic1@gmail.com>
2022-08-09 21:19:40 +0200

    implemented methods for filtering books

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -10,13 +10,62 @@ use Illuminate\Support\Facades\Hash;
 */
 class BookService
 {
+  /*
+    Used when filter criteria is ambigious,
+    Determines criteria and calls relevant method.
+  */
+  public function getAllorFiltered($category = null, $subcategory = null){
+
+    if(!empty($category) && empty($subcategory)){
+      return $this->getByCategory($category);
+    }
+
+    if(!empty($subcategory)){
+    return $this->getBySubCategory($subcategory);
+    }
+
+    if(empty($category) && empty($subcategory)){
+      return $this->getAll();
+    }
+
+  }
 
   /*
-   Returns a collection of n random recommended books
-   n = $quantity
+  Return all books eager loaded
+  */
+  public function getAll(){
+    return Book::with('authors', 'category')->get();
+  }
+
+  /*
+  Returns a collection of n random recommended books
+  n = $quantity
   */
   public function getRecommended(int $quantity){
-    return Book::with('authors')->where('isRecommended', 1)->get()->random($quantity);
+    return Book::with('authors')->inRandomOrder()->take($quantity)->get();
   }
+
+  /*
+  Returns a collection of books by passed categoryId
+  */
+  public function getBySubCategory(int $categoryId){
+    return Book::with('authors', 'category')->category($categoryId)->get();
+  }
+
+  /*
+  Returns a collection of books filtered by passed category
+  category in ['fiction','nonFiction']
+  */
+  public function getByCategory($category){
+
+    if($category === 'fiction'){
+      return Book::with('authors', 'category')->fiction()->get();
+    }
+
+    if($category === 'nonFiction'){
+      return Book::with('authors', 'category')->nonFiction()->get();
+    }
+  }
+
 
 }
