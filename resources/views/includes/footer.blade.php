6cff9ad9eb6733bcf03bc39de4258c66aac01d0d
David <dgudovic1@gmail.com>
2022-08-09 13:57:19 +0200

    built footer v1

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,30 @@
-<footer class="flex-col h-48 w-full px-40 py-10 bg-gray-800 relative bottom-0 ">
-  <div class="flex justify-between mb-20">
-    <p> Neki tekst </p>
-    <p> Neki drugi tekst</p>
-    <p> Adresa</p>
+<footer class="flex-col w-full h-fit px-10 py-6 bg-gray-800 text-white relative bottom-0 ">
+  <!-- Top footer -->
+  <div class="flex flex-wrap gap-11 justify-evenly">
+    <!-- Logo -->
+    <div>
+      <svg  class="fill-current" width="216" height="213" viewBox="0 0 216 213" xmlns="http://www.w3.org/2000/svg">
+      <path d="M178.246 143.236C161.682 161.039 143.855 177.119 126.007 189.422C103.065 205.104 78.8607 214.403 52.9735 212.724C27.485 211.024 -1.39165 197.042 1.15499 167.82C4.56527 113.594 87.3642 105.554 133.226 93.2507C208.828 73.3494 195.652 5.16398 127.712 5.16398C99.5438 5.16398 68.0984 20.3826 53.9479 45.3198C49.6961 52.8077 40.1961 55.414 33.0876 50.5547C25.0048 45.0327 24.6284 33.0831 32.4897 27.2519C58.8862 7.72618 95.0706 0.923096 127.712 0.923096C155.747 0.923096 213.921 6.86475 215.626 42.8681C216.047 56.8497 208.828 67.4299 191.843 76.751C175.699 85.6524 150.654 93.6924 115.399 102.594C10.9208 130.535 32.1575 247.027 123.46 185.6C156.389 163.247 185.554 128.812 201.852 107.564C206.325 101.732 215.604 104.935 215.604 112.268V212.702H178.246V143.236Z"/>
+    </svg>
+    </div>
+    <!-- end logo -->
+    <!-- Products-->
+    <div class="flex flex-col gap-10">
+      <p class="border-b-2">Pogledajte našu ponudu</p>
+      <p>Sve</p>
+      <p>Fikcija</p>
+      <p>Stručna literatura</p>
+    </div>
+    <!-- End products -->
+    <!-- Contact -->
+    <div class="flex flex-col gap-10">
+      <p class="border-b-2">Kontakt</p>
+      <p><i class="fa-solid fa-envelope"></i> dgudovic.dev@gmail.com</p>
+      <p><i class="fa-solid fa-phone"></i> 067/010-010</p>
+      <p> <i class="fa-brands fa-github"></i> <a href="https://github.com/DavidGudovic">David Gudović</a></p>
+    </div>
+    <!-- End contact -->
   </div>
-  <div class="flex justify-center">
-    <p>Copyright @2022</p>
-  </div>
+  <!-- End footer -->
+
 </footer>
