84fea6383e6e48fbb9c01f227cc39594db9114e7
David <dgudovic1@gmail.com>
2022-08-10 14:55:42 +0200

    added views for displaying user information

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.app')
+
+@section('content')
+  <div class="">
+    <p>Api response: List of these: {{auth()->user()}}</p>
+  </div>
+@endsection
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.app')
+
+@section('content')
+  <div class="">
+    <p>Api response: {{auth()->user()}}</p>
+  </div>
+@endsection
