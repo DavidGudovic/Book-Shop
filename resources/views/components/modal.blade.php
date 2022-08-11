df62ee72f8c292cf5617c088189de917bbf12858
David <dgudovic1@gmail.com>
2022-08-11 13:31:34 +0200

    formatting

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -3,13 +3,10 @@
   x-show="show"
   x-on:keydown.escape.window="show = false"
   class="flex justify-center fixed inset-0 overflow-y-auto px-4 py-6 md:py-24 z-10">
-  <div x-show="show" class="fixed inset-0 transform" x-on:click="show=false">
-      <div class="absolute inset-0 bg-gray-500 opacity-50">
-      </div>
+  <div x-show="show" class="fixed inset-0 transform absolute inset-0 bg-gray-500 opacity-70" x-on:click="show=false">
   </div>
 
-   <div x-show="show" class="bg-white rounded-lg overflow-y-auto transform  w-full m-10 p-6 md:w-1/2 ">
+   <div x-show="show" class="bg-white rounded-lg overflow-y-auto transform  w-full m-10 p-6  ">
       {{ $slot }}
-
    </div>
 </div>
