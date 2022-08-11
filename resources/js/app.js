e23e6e0875fc2c85c84434c41c14f6a6c5f8be91
David <dgudovic1@gmail.com>
2022-08-11 20:47:06 +0200

    added alpinejs focus plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
 import '../css/app.css';
 import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+
 window.Alpine = Alpine;
+Alpine.plugin(focus)
 Alpine.start();
