28aa6bea44b1cc85124f5cea23839c1a2c7f88ce
David <dgudovic1@gmail.com>
2022-08-11 14:22:37 +0200

    Cleaned up if statements with ternary operator

--- app/Http/Middleware/AuthorizeAdmin.php
+++ app/Http/Middleware/AuthorizeAdmin.php
@@ -9,16 +9,9 @@ class AuthorizeAdmin
 {
     /**
      * If user making request isnt admin, throw 403 Unauthorized
-     *s
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-      if(!(auth()->user()->role == 'admin')){
-        abort(403);
-      }
-        return $next($request);
+       return auth()->user()->role == 'admin' ? return $next($request) : abort(403);
     }
 }
