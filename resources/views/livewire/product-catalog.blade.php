21a9abe045a1de94af15b0336bd98f91167032be
David <dgudovic1@gmail.com>
2022-08-12 10:33:50 +0200

    Added star rating placeholder

--- resources/views/livewire/product-catalog.blade.php
+++ resources/views/livewire/product-catalog.blade.php
@@ -2,6 +2,7 @@
   <!-- List of Books -->
   <div class="flex flex-row flex-wrap gap-20 justify-center">
     @forelse($book_list as $book)
+      <!-- Book -->
       <div class="flex flex-col gap-2">
         <img src="{{URL('/images/'. $book->image)}}" class="h-[420px] w-[270px]">
         <p class="font-semibold">{{$book->name}}</p>
@@ -15,12 +16,21 @@
           @endforeach
         </p>
         <!-- End Authors -->
+        <!-- Score -->
+        <div class="flex flex-row gap-5">
+          @foreach(range(1,5) as $index)
+                <i class="fa-solid fa-star"></i>
+          @endforeach
+        </div>
+
+        <!-- End score -->
         <div class="flex justify-between items-center mt-3">
           <a href="{{route('books.show', $book)}}" class="rounded-3xl bg-black text-white px-2 py-1" name="button">Više informacija</a>
           <p class="text-lg">{{$book->price}} RSD</p>
           <a href=""><i class="fa-solid fa-cart-shopping"></i></a>
         </div>
       </div>
+      <!--End book -->
     @empty
       <img src="{{URL('images/noresult.jpg')}}" alt="Nema rezultata pretrage">
     @endforelse
