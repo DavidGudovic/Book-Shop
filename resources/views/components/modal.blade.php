0423116734d5372cecc1e02a78abc275592f9f22
David <dgudovic1@gmail.com>
2022-08-12 23:42:13 +0200

    removed unnecessary elements from base modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,13 +1,16 @@
+<!-- Modal -->
 <div
   x-data="{ showModal: @entangle($attributes->wire('model')) }"
   x-show="showModal" x-trap.noscroll="showModal"
   x-on:keydown.escape.window="showModal = false"
   x-cloak
   class="flex justify-center fixed inset-0 px-4 py-6 md:py-24 z-50">
-  <div x-show="showModal" class="fixed inset-0 transform absolute inset-0 bg-gray-500 opacity-70 " x-on:click="showModal=false">
-  </div>
+  <!-- Modal Background -->
+  <div x-show="showModal" class="fixed inset-0 transform absolute inset-0 bg-gray-500 opacity-70 " x-on:click="showModal=false"></div>
+  <!-- End modal background-->
 
-   <div x-show="showModal" class="bg-white rounded-lg transform w-full md:w-1/2 p-6 z-50">
-      {{ $slot }}
-   </div>
+  <!-- Custom Modal body -->
+          {{ $slot }}
+   <!-- End custom modal body -->
 </div>
+ <!--End modal -->
