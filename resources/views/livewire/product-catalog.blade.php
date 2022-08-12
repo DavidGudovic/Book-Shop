3116ad837362b4cfe36ccae6bdafdc900356b4b4
David <dgudovic1@gmail.com>
2022-08-12 23:39:40 +0200

    changed links for images

--- resources/views/livewire/product-catalog.blade.php
+++ resources/views/livewire/product-catalog.blade.php
@@ -2,7 +2,7 @@
   <!-- Fixed -->
   <!-- Search loading indicator -->
   <div wire:loading class="fixed m-auto right-0 left-0 top-32 h-16 w-16">
-    <img src="{{URL('/images/loading.gif')}}" alt="">
+    <img src="{{URL('/images/util/loading.gif')}}" alt="">
   </div>
   <!-- End loading indicator -->
   <!-- End fixed-->
@@ -43,7 +43,7 @@
       </div>
       <!--End book -->
     @empty
-      <img src="{{URL('images/noresult.jpg')}}" alt="Nema rezultata pretrage">
+      <img src="{{URL('images/util/noresult.jpg')}}" alt="Nema rezultata pretrage">
     @endforelse
   </div>
   <!-- End of list-->
