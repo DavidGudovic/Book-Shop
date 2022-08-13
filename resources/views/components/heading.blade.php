cc5659f291bcf9808c68e5fec5e2c8361906b132
David <dgudovic1@gmail.com>
2022-08-13 16:05:47 +0200

    added injectable aurora heading

---
+++ resources/views/components/heading.blade.php
@@ -0,0 +1,7 @@
+<!--- Heading --->
+<div class="flex justify-center my-4">
+  <div class="flex flex-col justify-center bg-aurora-heading bg-cover bg-center w-64 h-32">
+    <h1 class="font-extrabold text-3xl text-center my-12  ">{{$slot}}</h1>
+  </div>
+</div>
+<!-- End heading -->
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,15 +2,18 @@
 
 @section('content')
 
-  <h1 class="font-extrabold text-3xl text-center mt-16 mb-4">Ponuda Knjiga</h1>
+  <x-heading>
+    Ponuda Knjiga
+  </x-heading>
+
   <!--Wrapper of products-->
   <div class="flex flex-col md:flex-row w-screen p-10 md:pr-6">
-    <!-- Responsive filters -->
+    <!-- Filters -->
     <div class="mb-6 md:mb-0">
       <livewire:filters :category_list="$filters">
     </div>
     <!-- End filter -->
-    <!-- Products display -->
+    <!-- Products catalog -->
     <div class="flex-1">
       <livewire:product-catalog :book_list="$books">
     </div>
