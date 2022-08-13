939bf9a3f76140dc1e7df6b594cf53e5f0e839b4
David <dgudovic1@gmail.com>
2022-08-14 01:17:14 +0200

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,17 @@ use App\Http\Controllers\Resources\ReclamationController;
 
 // home page page
 Route::get('/', [HomePageController::class, 'index'])->name('home');
-
 //Authentication
 Route::resource('register', RegisterController::class, ['only' => ['create', 'store']]);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);  
+Route::post('/login', [LoginController::class, 'store']);
 Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
-
 /*
 Routes for displaying lists of products
 catalog/ route fixes books.show hijacking requests from books.index/param/null
 */
 Route::get('catalog/{category?}/{subcategories?}', [BookController::class, 'index'])->name('books.index');
 Route::resource('books', BookController::class, ['except' => 'index']);
-
 /*
 Routes only logged in users can access
 redirects to login page if unauthorized
@@ -34,15 +31,12 @@ redirects to login page if unauthorized
 Route::middleware('auth', 'private')->group(function () {
 Route::get('/user/{user}/delete', [UserController::class, 'delete'])->name('user.delete');
 Route::resource('user', UserController::class);
-Route::resource('orders', OrderController::class);
-Route::resource('reclamations', ReclamationController::class);
+Route::resource('user.orders', OrderController::class);
+Route::resource('user.reclamations', ReclamationController::class);
 });
-
 /*
 Routes only administrators can access
 Throws 403 if unauthorized
 */
 Route::middleware('admin')->prefix('administration')->group(function () {
-
-
 });
