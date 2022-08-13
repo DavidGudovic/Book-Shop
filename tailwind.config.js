1711f4050cb4b933ae28e6b006bb15b210489e51
David <dgudovic1@gmail.com>
2022-08-13 16:01:47 +0200

    Moved image includes to tailwind extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-pattern': "url('/images/util/header_pattern.png')",
+        'aurora-login': "url('/images/auroras/aurora-login.png')",
+        'aurora-login-phone': "url('/images/auroras/aurora-login-phone.png')",
+        'aurora-register': "url('/images/auroras/aurora-register.png')",
+        'aurora-register-phone': "url('/images/auroras/aurora-register-phone.png')",
+        'aurora-heading' : "url('/images/auroras/aurora-heading.png')",
+      }
+    },
   },
   plugins: [],
 }
