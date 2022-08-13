a3de11c26c62e31342210ec443efef5379f52564
David <dgudovic1@gmail.com>
2022-08-14 01:18:09 +0200

    fixed oversight from previous commit

--- app/Http/Middleware/PrivateInfo.php
+++ app/Http/Middleware/PrivateInfo.php
@@ -12,6 +12,6 @@ class PrivateInfo
      */
     public function handle(Request $request, Closure $next)
     {
-        return  $request->user() == auth()->user() ? $next($request) : abort(403);
+        return  $request->route('user') == auth()->user() ? $next($request) : abort(403);
     }
 }
