046fdb1cb295f8d39b41cd2b87e20599315f3acf
David <dgudovic1@gmail.com>
2022-08-14 01:18:42 +0200

    changed order of options in select

--- resources/views/reclamations/index.blade.php
+++ resources/views/reclamations/index.blade.php
@@ -10,11 +10,10 @@
   <div class="flex flex-col border border-black px-8 md:w-72">
     <form class="flex flex-col gap-1 py-4 "action="index.html" method="post">
       <p class="font-bold text-center">Filteri</p>
-
       <p>Status</p>
       <select class="" name="">
-        <option value="">Refundirana</option>
         <option value="">Obrađuje se</option>
+        <option value="">Refundirana</option>
         <option value="">Odbijena</option>
       </select>
       <div class="flex flex-row gap-6 mt-4">
