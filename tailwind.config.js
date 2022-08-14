747106b143779ce66da30d247d1c069f0cd198c7
David <dgudovic1@gmail.com>
2022-08-14 12:50:02 +0200

    Added text animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ module.exports = {
         'aurora-heading' : "url('/images/auroras/aurora-heading.png')",
         'aurora-phone': "url('/images/auroras/aurora-phone.png')",
       },
+
+      keyframes: {
+        slideInFromLeft: {
+          '0% '  :{transform: 'translateX(-100%) '},
+          '100% ':{transform: 'translateX(0)' },
+        },
+        slideInFromTop: {
+          '0% '  :{transform: 'translateY(-100%) '},
+          '100% ':{transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'apear-from-left': 'slideInFromLeft 1.5s',
+        'apear-from-top': 'slideInFromTop 1.5s',
+      },
     },
   },
   plugins: [],
