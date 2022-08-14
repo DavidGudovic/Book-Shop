697bf4d638e28270aec2d36425414c46c7e8c23e
David <dgudovic1@gmail.com>
2022-08-14 18:25:54 +0200

    implemented factories

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,22 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
- */
+* @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+*/
 class ReviewFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+  * Define the model's default state.
+  *
+  * @return array<string, mixed>
+  */
+  public function definition()
+  {
+    return[
+      'user_id' => User::all()->random()->id,
+      'book_id' => Book::all()->random()->id,
+      'text' => fake()->paragraph,
+      'score' => rand(1,5),
+    ];
+  }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,18 @@
 <?php
 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    /*
+     Only seeds Users, Reclamations and Reviews
+    */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory(20)->create();
+         \App\Models\Review::factory(100)->create();
+         //\App\Models\Reclamation::factory(100)->create();
+         //\App\Models\Order::factory(100)->create();
     }
 }
