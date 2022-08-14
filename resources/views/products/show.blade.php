0767b0788c09e87648e503acdff29a96efe61a27
David <dgudovic1@gmail.com>
2022-08-14 13:44:56 +0200

    added recensions

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,7 +7,7 @@
     <!-- Book -->
     <div x-data='{quantity: 1}' class="flex flex-col md:flex-row border-2 border-black px-4 md:pr-0 py-6">
       <!-- Info -->
-      <div class="flex flex-col md:flex-row pb-4 md:pb-0 border-b md:border-b-0 md:border-r border-black  md:w-1/2 md:min-w-[500px] ">
+      <div class="flex flex-col border-b border-black pb-4 px-6 md:px-0 md:flex-row md:pb-0 md:w-1/2 md:min-w-[500px] md:border-b-0 md:border-r ">
         <img src="{{URL('/images/' . $book->image)}}" alt="Cover image of {{$book->title}} "
              class="h-full max-h-[600px] max-w-[400px] w-full md:h-[400px] md:w-[250px] mb-3 md:mb-0 md:mr-6">
         <div class="flex flex-col justify-between gap-3 md:gap-6">
@@ -70,16 +70,16 @@
       </x-heading>
       <!-- Actions-->
 
-        <div class="flex flex-col md:flex-row gap-6 justify-center px-10 md:px-0">
+        <div class="flex flex-col md:flex-row gap-6 justify-center px-10 mb-5 md:px-0">
           <p class="pt-0.5">Sortiraj po:</p>
           <div class="flex flex-row justify-between md:gap-6">
 
-            <select class="" name="">
+            <select class="" name="filterby">
               <option value="">Oceni</option>
               <option value="">Datumu</option>
             </select>
 
-            <select class="" name="">
+            <select class="" name="filterdirection">
               <option value="">Opadajuće</option>
               <option value="">Rastuće</option>
             </select>
@@ -92,6 +92,28 @@
       <!-- End actions -->
 
       <!-- List-->
+      <div class="flex flex-col items-center justify-center gap-4">
+        @forelse([1,2,3,4,5,6,7,8,9] as $index)
+          <div class="flex flex-col gap-5 border-b border-black">
+             <p>{{$index}} Korisnik</p>
+             <!-- Score -->
+             <div class="flex flex-row gap-5">
+               @if($book->average_score > 0)
+                 @foreach(range(1, $book->average_score) as $index)
+                   <i class="fa-solid fa-star"></i>
+                 @endforeach
+               @else
+                 <p class="text-gray-500 text-sm"> Još uvek nema ocena </p>
+               @endif
+             </div>
+             <!-- End score -->
+             <p>{{$index}} -> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        @empty
+          <img src="{{URL('/images/util/noresult.jpg')}}" alt="" class="w-[400px] md:w-[500px] my-4">
+        @endforelse
+      </div>
+
       <!-- End List-->
 
     </div>
