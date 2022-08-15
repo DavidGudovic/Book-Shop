515f793ca52697412498d629e34322a18eaa00ef
David <dgudovic1@gmail.com>
2022-08-15 10:05:17 +0200

    added reviewed

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -4,24 +4,39 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use App\Models\Book;
+use App\Models\Review;
+use App\Mdoels\User;
 
-    /*
-    Custom blade directives
-    */
+/*
+Custom blade directives
+*/
 class BladeServiceProvider extends ServiceProvider
 {
 
-    public function register()
-    {
-        //
-    }
+  public function register()
+  {
+    //
+  }
 
 
-    public function boot()
-    {
-         // @admin --html to render if user is admin-- @endadmin
-          Blade::if('admin', function() {
-            return auth()->user() && auth()->user()->role === 'admin';
-          });
-    }
+  public function boot()
+  {
+    // @admin --html to render if user is admin-- @endadmin
+    Blade::if('admin', function() {
+      return auth()->user() && auth()->user()->role === 'admin';
+    });
+
+    // @reviewed($book) --html to render if has reviewed -- @endreviewed
+    Blade::if('reviewed', function (Book $book) {
+
+      foreach(auth()->user()->reviews as $review){
+        if($review->book_id == $book->id){
+          return true;
+        }
+      }
+      return false;
+    });
+
+  }
 }
