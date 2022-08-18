8093f3c26ae8b59a34452b2ea78655e4be76af12
David <dgudovic1@gmail.com>
2022-08-18 15:42:52 +0200

    restify

--- app/Http/Controllers/Admin/ReclamationController.php
+++ app/Http/Controllers/Admin/ReclamationController.php
@@ -12,7 +12,7 @@ class ReclamationController extends Controller
      */
     public function index()
     {
-        return view('admin.reclamations');
+        return view('admin.reclamations.index');
     }
 
     /**
