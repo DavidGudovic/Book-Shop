bd1f5bb2e078988e9b0056ea811a1f913deefced
David <dgudovic1@gmail.com>
2022-08-18 09:27:19 +0200

    made variable previously hardcoded values

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -11,8 +11,8 @@ use App\Services\CategoryService;
 class HomePageController extends Controller
 {
   // Gets relevant data and forwards it to home page view
-  public function index(BookService $bookService , CategoryService $categoryService ){
-    return view('index', ['recommends' => $bookService->getRecommended(3),
+  public function index(BookService $bookService , CategoryService $categoryService, int $recommendedCount = 3 ){
+    return view('index', ['recommends' => $bookService->getRecommended($recommendedCount),
                           'fictionCategories' => $categoryService->getAll('fiction'),
                           'nonFictionCategories' => $categoryService->getAll('nonFiction')]);
   }
